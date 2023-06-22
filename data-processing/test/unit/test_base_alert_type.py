db7c8090178e18b95234e3d62f300697838c3a8d
Timothy Standen <timothyf1@live.co.uk>
2023-06-22 11:58:50 +0100

    Sort road tests

--- data-processing/test/unit/test_base_alert_type.py
+++ data-processing/test/unit/test_base_alert_type.py
@@ -30,8 +30,27 @@ class TestAlertType(unittest.TestCase):
         assert len(roads_sorted["single"]) == 0
         assert len(roads_sorted["multi"]) == 4
 
+    def test_sort_roads_2(self):
+        G = map_graph("test/osm-data/graph/map.osm")
+
         roads = G.edges(-101800, data=True)
         roads_sorted = AlertType.sort_roads(roads)
 
         assert len(roads_sorted["single"]) == 1
         assert len(roads_sorted["multi"]) == 2
+
+    def test_sort_roads_3(self):
+        G = map_graph("test/osm-data/graph/map_2_roads_with_lanes_att.osm")
+
+        roads = G.edges(-102069, data=True)
+        roads_sorted = AlertType.sort_roads(roads)
+
+        assert len(roads_sorted["single"]) == 2
+        for road in roads_sorted["single"]:
+            assert road[2]["lanes"] == "1"
+        assert len(roads_sorted["multi"]) == 2
+        for road in roads_sorted["multi"]:
+            assert road[2]["lanes"] == "2"
+
+    def test_alert_locations(self):
+        assert AlertType.alert_locations("a", "b") == None
