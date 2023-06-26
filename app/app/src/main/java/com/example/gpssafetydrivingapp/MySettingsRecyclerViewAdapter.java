848224397221aaf6295115d31ea46b754a47ef11
Timothy Standen <timothyf1@live.co.uk>
2023-06-26 14:28:40 +0100

    Basic fragment structure for navigation

---
+++ app/app/src/main/java/com/example/gpssafetydrivingapp/MySettingsRecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package com.example.gpssafetydrivingapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.gpssafetydrivingapp.placeholder.PlaceholderContent.PlaceholderItem;
+import com.example.gpssafetydrivingapp.databinding.FragmentSettingsBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MySettingsRecyclerViewAdapter extends RecyclerView.Adapter<MySettingsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public MySettingsRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentSettingsBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentSettingsBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
