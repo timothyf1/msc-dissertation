999782429a744102e5da42b11462fc19b58aa368
Timothy Standen <timothyf1@live.co.uk>
2023-07-07 10:10:25 +0100

    Check to see if location has been turned off while alerts are active

--- app/app/src/main/java/com/example/gpssafetydrivingapp/alerts/AlertCheckerWorker.java
+++ app/app/src/main/java/com/example/gpssafetydrivingapp/alerts/AlertCheckerWorker.java
@@ -5,9 +5,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.location.LocationManagerCompat;
 import androidx.preference.PreferenceManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
@@ -16,6 +18,7 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.concurrent.TimeUnit;
@@ -24,6 +27,7 @@ public class AlertCheckerWorker extends Worker {
 
     private FusedLocationProviderClient fusedLocationClient;
     private SharedPreferences sharedPreferences;
+    int count = 100;
 
     public AlertCheckerWorker(Context appContext, WorkerParameters workerParams) {
         super(appContext, workerParams);
@@ -48,23 +52,40 @@ public class AlertCheckerWorker extends Worker {
         }
 
         while (sharedPreferences.getBoolean("switch_alerts_enable", false)) {
-            Task<Location> taskLocation = fusedLocationClient.getCurrentLocation(Priority.PRIORITY_LOW_POWER, null);
+
+            // Check to see if location has been turned off
+            if (! LocationManagerCompat.isLocationEnabled((LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE))) {
+                WorkerUtils.makeStatusNotification("Location is unavailable", "Alerts have been turned off", getApplicationContext(), 57);
+                AlertChecker.stopAlertChecker(getApplicationContext());
+                return Result.failure();
+            }
+
+            Task<Location> taskLocation = fusedLocationClient.getCurrentLocation(Priority.PRIORITY_PASSIVE, null);
+            fusedLocationClient.flushLocations();
+
+            taskLocation.addOnFailureListener((new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    AlertChecker.stopAlertChecker(getApplicationContext());
+                    WorkerUtils.makeStatusNotification("Location failed", "Alerts have been turned off", getApplicationContext(), 57);
+                }
+            }));
 
             taskLocation.addOnCompleteListener((new OnCompleteListener<Location>() {
                 @Override
                 public void onComplete(@NonNull Task<Location> task) {
                     Location location = task.getResult();
-                    String out = "Latitude: " + location.getLatitude() +
-                            " Longitude" + location.getLongitude() +
-                            " Bearing" + location.getBearing() +
-                            " Speed" + location.getSpeed();
+                    String out = count++ + "Latitude: " + location.getLatitude() +
+                            " Longitude: " + location.getLongitude() +
+                            " Bearing:" + location.getBearing() +
+                            " Speed: " + location.getSpeed();
 
-                    WorkerUtils.makeStatusNotification(out, getApplicationContext(), 56);
+                    WorkerUtils.makeStatusNotification("Location", out, getApplicationContext(), 56);
                 }
             }));
 
             try {
-                TimeUnit.SECONDS.sleep(10);
+                TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
--- app/app/src/main/java/com/example/gpssafetydrivingapp/alerts/WorkerUtils.java
+++ app/app/src/main/java/com/example/gpssafetydrivingapp/alerts/WorkerUtils.java
@@ -1,49 +1,55 @@
 package com.example.gpssafetydrivingapp.alerts;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
 import com.example.gpssafetydrivingapp.R;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 final class WorkerUtils {
 
-    static void makeStatusNotification(String message, Context context, int id) {
+    static void makeStatusNotification(String title, String message, Context context, int id) {
 
         // Make a channel if necessary
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
-            CharSequence name = "Driving Alerts";
-            String description = "Show notification when alerts are active";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel =
-                    new NotificationChannel("ALERTS", name, importance);
-            channel.setDescription(description);
+        CharSequence name = "Driving Alerts";
+        String description = "Show notification when alerts are active";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel =
+                new NotificationChannel("ALERTS", name, importance);
+        channel.setDescription(description);
 
-            // Add the channel
-            NotificationManager notificationManager =
-                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // Add the channel
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(channel);
-            }
+        if (notificationManager != null) {
+            notificationManager.createNotificationChannel(channel);
         }
 
         // Create the notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ALERTS")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("Test location")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setVibrate(new long[0])
-                .setShowWhen(false);
+                .setContentTitle(title)
+                .setContentText(message);
 
         // Show the notification
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         NotificationManagerCompat.from(context).notify(id, builder.build());
     }
 
