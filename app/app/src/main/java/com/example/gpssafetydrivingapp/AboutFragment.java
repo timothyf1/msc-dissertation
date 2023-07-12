3d20317d59c9f1f3535fec68a20f1e6a195021dc
Timothy Standen <timothyf1@live.co.uk>
2023-07-12 11:49:56 +0100

    Added about fragment

---
+++ app/app/src/main/java/com/example/gpssafetydrivingapp/AboutFragment.java
@@ -0,0 +1,40 @@
+package com.example.gpssafetydrivingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AboutFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+//    public static AboutFragment newInstance(String param1, String param2) {
+//        AboutFragment fragment = new AboutFragment();
+//        Bundle args = new Bundle();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
